43d6f19d0e12ab07da0d83e1690dbc0e5c96b19c
Таня Волкова <43178005+assrichiacoli@users.noreply.github.com>
2018-09-18 16:04:25 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -18,15 +18,8 @@ def transposeDict(d):
     
 
 def mex(e):
-    a = []
-    for i in e:
-        try:
-            a.append(int(i))
-        except ValueError:
-            continue
-        a.sort()
     k=1
-    while k in a:
+    while k in e:
         k+=1
     else:
         return (k)
