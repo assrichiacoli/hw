4349baea0cbeef57b528f1d764d69f5aba22b571
Таня Волкова <43178005+assrichiacoli@users.noreply.github.com>
2018-10-03 17:03:11 +0300

    Add files via upload

---
+++ task1 - rewritten.py
@@ -0,0 +1,48 @@
+iodef unique(e):
+    a = []
+    for i in e:
+        if i not in a:
+            a.append(i)
+            a.sort()
+    return a
+
+
+def transposeDict(d):
+    a = d.keys()
+    b = d.values()
+    new = dict(zip(b, a))
+    return new
+
+
+def mex(e):
+    k = 1
+    while k in e:
+        k += 1
+    else:
+        return(k)
+
+
+def frequencyDict(s):
+    a = []
+    b = []
+    for i in s:
+        if i not in a:
+            a.append(i)
+    for j in a:
+        c = s.count(j)
+        b.append(c)
+    new = dict(zip(a, b))
+    return new
+
+
+assert unique([1, 2, 1, 3]) == [1, 2, 3]
+assert unique({5, 1, 3}) == [1, 3, 5]
+assert unique('adsfasdf') == ['a', 'd', 'f', 's']
+assert transposeDict({1: 'a', 2: 'b'}) == {'a': 1, 'b': 2}
+assert transposeDict({1: 1}) == {1: 1}
+assert transposeDict({}) == {}
+assert mex([1, 2, 3]) == 4
+assert mex(['asdf', 123]) == 1
+assert mex([0, 0, 1, 0]) == 2
+assert frequencyDict('') == {}
+assert frequencyDict('abacaba') == {'a': 4, 'b': 2, 'c': 1}
---
+++ task2.py
@@ -0,0 +1,83 @@
+def listToString(a):
+    return str(a)
+
+
+def addBorder(a):
+    o = '-'
+    b = o * int(len(a[1]))
+    border = ['+' + b + '+']
+    for i in a:
+        border.append('|' + i + '|')
+    border.append('+' + b + '+')
+    return border
+
+
+def shorting(e):
+    a = []
+    for i in e:
+        if len(i) <= 10:
+            a.append(i)
+        else:
+            a.append(i[0] + str(int(len(i) - 2)) + i[-1])
+    return a
+
+
+def competition(e, k):
+    a = []
+    for ind, i in enumerate(e):
+        if ind <= k:
+            a.append(i)
+        elif i == a[-1]:
+            a.append(i)
+    b = []
+    for item in a:
+        if item != 0:
+            b.append(item)
+    return len(b)
+
+
+def goodPairs(a, b):
+    n = []
+    for i in a:
+        for j in b:
+            if (i * j) % (i + j) == 0:
+                s = i ** 2 + j ** 2
+                n.append(s)
+    n.sort()
+    return n
+
+
+def makeShell(n):
+    a = []
+    o = [0]
+    k = 1
+    while k <= n:
+        a.append(o*k)
+        k += 1
+    m = n - 1
+    while m <= n and m > 0:
+        a.append(o*m)
+        m -= 1
+        assert listToString([]) == "[]"
+    return a
+
+
+assert listToString([1, 2, 3]) == "[1, 2, 3]"
+assert listToString([-5]) == "[-5]"
+assert competition([5, 4, 3, 2, 1], 2) == 3
+assert competition([1, 0, 0, 0], 3) == 1
+assert competition([10, 9, 8, 7, 7, 7, 5, 5], 4) == 6
+assert goodPairs([4, 5, 6, 7, 8], [8, 9, 10, 11, 12]) == [128, 160, 180]
+assert goodPairs([2], [2]) == [8]
+assert goodPairs([7, 8, 9], [5, 3, 2]) == []
+assert makeShell(1) == [[0]]
+assert makeShell(2) == [[0], [0, 0], [0]]
+assert makeShell(3) == [[0], [0, 0], [0, 0, 0], [0, 0], [0]]
+assert addBorder(['abc',
+                  'def']) == ['+---+',
+                              '|abc|',
+                              '|def|',
+                              '+---+']
+assert shorting(['word', 'localization', 'internationalization',
+'pneumonoultramicroscopicsilicovolcanoconiosis']) ==
+['word', 'l10n', 'i18n', 'p43s']
