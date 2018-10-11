85d38247cab8f1139d373daeab6cf555075bc1ab
Таня Волкова <43178005+assrichiacoli@users.noreply.github.com>
2018-10-11 08:56:30 +0300

    Update task1 - rewritten.py

--- task1 - rewritten.py
+++ task1 - rewritten.py
@@ -1,4 +1,4 @@
-iodef unique(e):
+def unique(e):
     a = []
     for i in e:
         if i not in a:
@@ -35,14 +35,17 @@ def frequencyDict(s):
     return new
 
 
-assert unique([1, 2, 1, 3]) == [1, 2, 3]
-assert unique({5, 1, 3}) == [1, 3, 5]
-assert unique('adsfasdf') == ['a', 'd', 'f', 's']
-assert transposeDict({1: 'a', 2: 'b'}) == {'a': 1, 'b': 2}
-assert transposeDict({1: 1}) == {1: 1}
-assert transposeDict({}) == {}
-assert mex([1, 2, 3]) == 4
-assert mex(['asdf', 123]) == 1
-assert mex([0, 0, 1, 0]) == 2
-assert frequencyDict('') == {}
-assert frequencyDict('abacaba') == {'a': 4, 'b': 2, 'c': 1}
+if __name__ == '__main__':
+    assert unique([1, 2, 1, 3]) == [1, 2, 3]
+    assert unique({5, 1, 3}) == [1, 3, 5]
+    assert unique('adsfasdf') == ['a', 'd', 'f', 's']
+    assert transposeDict({1: 'a', 2: 'b'}) == {'a': 1, 'b': 2}
+    assert transposeDict({1: 1}) == {1: 1}
+    assert transposeDict({}) == {}
+    assert mex([1, 2, 3]) == 4
+    assert mex(['asdf', 123]) == 1
+    assert mex([0, 0, 1, 0]) == 2
+    assert frequencyDict('') == {}
+    assert frequencyDict('abacaba') == {'a': 4, 'b': 2, 'c': 1}
+    print('Tests were executed successfully')
+
